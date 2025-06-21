52319967fd802d457dec5afa90266d68c7fa2af6
ARUQUIPA FLORES ALAN NAHUEL <aaruquipaflores639@alumno.unlam.edu.ar>
2025-06-21 19:17:25 -0300

    feat: implementing client for accounts and sending email

--- src/main/java/ar/edu/unlam/tpi/notifications/dto/request/AccountDetailRequest.java
+++ src/main/java/ar/edu/unlam/tpi/notifications/dto/request/AccountDetailRequest.java
@@ -1,10 +1,14 @@
 package ar.edu.unlam.tpi.notifications.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccountDetailRequest {
     private Long userId;
     private String type;
--- src/main/java/ar/edu/unlam/tpi/notifications/service/impl/NotificationServiceImpl.java
+++ src/main/java/ar/edu/unlam/tpi/notifications/service/impl/NotificationServiceImpl.java
@@ -1,9 +1,16 @@
 package ar.edu.unlam.tpi.notifications.service.impl;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import ar.edu.unlam.tpi.notifications.clients.AccountsClient;
+import ar.edu.unlam.tpi.notifications.dto.request.AccountDetailRequest;
 import ar.edu.unlam.tpi.notifications.dto.request.NotificationCreateRequest;
+import ar.edu.unlam.tpi.notifications.dto.response.AccountDetailResponse;
 import ar.edu.unlam.tpi.notifications.dto.response.NotificationCreateResponse;
+import ar.edu.unlam.tpi.notifications.dto.response.UserResponse;
 import ar.edu.unlam.tpi.notifications.models.Notification;
 import ar.edu.unlam.tpi.notifications.persistence.dao.NotificationDAO;
 import ar.edu.unlam.tpi.notifications.service.EmailService;
@@ -18,14 +25,52 @@ import lombok.extern.slf4j.Slf4j;
 public class NotificationServiceImpl implements NotificationService {
     private final NotificationDAO notificationDAO;
     private final EmailService emailService;
+    private final AccountsClient accountsClient;
 
     @Override
     public NotificationCreateResponse saveNewNotification(NotificationCreateRequest request){
         log.info("Creating new notification with request: {}", request);
-        Notification notification  = Converter.convertToEntity(request, Notification.class);        
+        Notification notification = instanceNewNotification(request);
         Notification result = notificationDAO.save(notification);
-        emailService.sendEmail("alanaruquipa242@gmail.com", "prueba");
+
+        checkIfWeNeedSendEmail(request);
         return Converter.convertToDto(result, NotificationCreateResponse.class);
     }
     
+    private Notification instanceNewNotification(NotificationCreateRequest request) {
+        Notification notification  = Converter.convertToEntity(request, Notification.class); 
+        notification.setIsRead(false);
+        notification.setCreatedAt(LocalDate.now());
+
+        return notification;
+    }
+
+    private void checkIfWeNeedSendEmail(NotificationCreateRequest request){
+        if(request.getInMail()){
+            log.info("Sending email notification to user with id: {}", request.getUserId());
+            AccountDetailResponse userInfo = searchUserInformation(request); 
+            emailService.sendEmail(userInfo.getEmail(), "prueba");
+        } else {
+            log.info("Email notification not sent for user with id: {}", request.getUserId());
+        }
+
+    }
+
+    private AccountDetailResponse searchUserInformation(NotificationCreateRequest request){
+        log.info("Searching user information for user with id: {}", request.getUserId());
+        UserResponse userInfo = getUserFromAccountClient(request);
+    
+        return switch(request.getUserType()) {
+            case "SUPPLIER" -> userInfo.getApplicants().get(0);
+            case "APPLICANT" -> userInfo.getSuppliers().get(0);
+            default -> userInfo.getWorkers().get(0);
+        };
+    }
+
+    private UserResponse getUserFromAccountClient(NotificationCreateRequest request){
+        log.info("Requesting user information from accounts client for user with id: {}", request.getUserId());
+        List<AccountDetailRequest> requestClient = List.of(new AccountDetailRequest(request.getUserId(), request.getUserType()));
+        UserResponse userResponse = accountsClient.getAccountById(requestClient);
+        return userResponse;
+    }
 }
